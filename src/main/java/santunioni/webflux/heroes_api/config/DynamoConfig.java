81a19b91da5076be30d6fa9408e5367a2db887a7
Vinícius Vargas <60983144+santunioni@users.noreply.github.com>
2021-03-16 18:25:05 -0300

    I changed deprecated usages in DynamoConfig class

--- src/main/java/santunioni/webflux/heroes_api/config/DynamoConfig.java
+++ src/main/java/santunioni/webflux/heroes_api/config/DynamoConfig.java
@@ -1,43 +1,52 @@
 package santunioni.webflux.heroes_api.config;
 
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.StringUtils;
 
 @Configuration
 @EnableDynamoDBRepositories
 public class DynamoConfig {
-  @Value("${amazon.dynamodb.endpoint}")
-  private String amazonDynamoDBEndpoint;
+    @Value("${amazon.dynamodb.endpoint}")
+    private String amazonDynamoDBEndpoint;
 
-  @Value("${aws_access_key_id}")
-  private String amazonAWSAccessKey;
+    @Value("${amazon.dynamodb.signingregion}")
+    private String amazonDynamoDBSigningRegion;
 
-  @Value("${aws_secret_access_key}")
-  private String amazonAWSSecretKey;
+    @Value("${aws_access_key_id}")
+    private String amazonAWSAccessKey;
 
-  @Bean
-  public AmazonDynamoDB amazonDynamoDB() {
-    AmazonDynamoDB amazonDynamoDB
-      = new AmazonDynamoDBClient(amazonAWSCredentials());
+    @Value("${aws_secret_access_key}")
+    private String amazonAWSSecretKey;
 
-    if (!StringUtils.isEmpty(amazonDynamoDBEndpoint)) {
-      amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        var amazonDynamoDB
+                = AmazonDynamoDBClientBuilder
+                .standard()
+                .withCredentials(
+                        new AWSStaticCredentialsProvider(amazonAWSCredentials())
+                );
+        if (!amazonDynamoDBEndpoint.isEmpty()) {
+            amazonDynamoDB.setEndpointConfiguration(
+                    new AwsClientBuilder.EndpointConfiguration(
+                            amazonDynamoDBEndpoint, amazonDynamoDBSigningRegion
+                    ));
+        }
+        return amazonDynamoDB.build();
     }
 
-    return amazonDynamoDB;
-  }
-
-  @Bean
-  public AWSCredentials amazonAWSCredentials() {
-    return new BasicAWSCredentials(
-      amazonAWSAccessKey, amazonAWSSecretKey);
-  }
+    @Bean
+    public AWSCredentials amazonAWSCredentials() {
+        return new BasicAWSCredentials(
+                amazonAWSAccessKey, amazonAWSSecretKey);
+    }
 
 }
