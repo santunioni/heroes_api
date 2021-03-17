2652a0a0adea37651a25685ab40bdcae7d75c0fa
Vinícius Vargas <60983144+santunioni@users.noreply.github.com>
2021-03-17 15:18:18 -0300

    I changed the docker script

--- start-on-docker.sh
+++ start-on-docker.sh
@@ -1,19 +1,11 @@
-# This script will take time the first time you runt it because
+# This script will take longer the first time you runt it because
 # it will download several docker images for building the Gradle app
 # into the container.
 
-docker run -d \
--p 8000:8000 \
--h DynamoDB-Heroes \
--v /home/dynamodblocal/data \
--w /home/dynamodblocal \
--t amazon/dynamodb-local:latest \
--jar DynamoDBLocal.jar -sharedDb -optimizeDbBeforeStartup -dbPath ./data \
+trap "docker-compose down" SIGINT
+
+docker network create --subnet=172.197.0.0/16 heroes_api_network
 
 ./gradlew bootBuildImage --imageName=heroes_api
 
-docker run \
--e "SPRING_PROFILES_ACTIVE=dev" \
--h APP-Heroes \
--p 8080:8080 \
--t heroes_api
+docker-compose up
